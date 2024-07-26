f6d8a4542ac96703c1229963e9f51f6e02ef9748
xEmmaV99 <emma.vancayseele@gmail.com>
2024-07-26 08:41:24 +0200

    added exp to testing_script.py

--- libs.py
+++ libs.py
@@ -1,6 +1,5 @@
 from source import *
 
-
 # u names = v, I , V, gamma, omega, f
 
 def get_custom_library_funcs(type='default'):
@@ -8,12 +7,32 @@ def get_custom_library_funcs(type='default'):
     all_but_gam = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14]
     lib_2nd_order = ps.PolynomialLibrary(degree=2, include_interaction=True)  # first order interaction
     lib_higher_order = ps.PolynomialLibrary(degree=5, include_interaction=False)
+    library_functions=[
+        lambda x: np.exp(np.abs(x/1000)),
+        lambda x: np.exp(-np.abs(x)),
+        lambda x: np.log(np.abs(x+.1)),
+    ]
+    function_names = [
+        lambda x: '\\exp{x}',
+        lambda x: '\\exp{-x}',
+        lambda x: '\\log{x}',
+    ]
+
+
+    lib_exp = ps.CustomLibrary(library_functions=library_functions, function_names=function_names, interaction_only=False)
 
     if type == 'default':
         inputs_per_library = [all_but_gam, [12]]
         custom_lib = ps.GeneralizedLibrary([lib_2nd_order,
                                            ps.FourierLibrary(n_frequencies=10, include_cos=True, include_sin=True)],
                                            tensor_array=None,  # don't merge the libraries
+                                           inputs_per_library=inputs_per_library)
+    elif type == 'exp':
+        inputs_per_library = [all_but_gam, all_but_gam, [12]]
+        custom_lib = ps.GeneralizedLibrary([lib_2nd_order,
+                                           lib_exp,
+                                           ps.FourierLibrary(n_frequencies=10, include_cos=True, include_sin=True)],
+                                           tensor_array=None,
                                            inputs_per_library=inputs_per_library)
     else:
         inputs_per_library = [all_but_gam, all_but_gam, [12]]
---
+++ testing_script.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.metrics import mean_squared_error
+from libs import *
+from source import *
+from prepare_data import *
+
+path_to_data_files = 'C:/Users/emmav/PycharmProjects/SINDY_project/data/Combined/07-20-load'
+
+# get the data
+xdot_train, x_train, u_train, xdot_val, x_val, u_val, TESTDATA = prepare_data(path_to_data_files, t_end = 2.5)
+
+# Fit the model
+library = ps.PolynomialLibrary(degree=2, include_interaction=True)
+library = get_custom_library_funcs('exp')
+threshold = 10
+
+opt = ps.SR3(thresholder="l1", threshold=threshold)
+
+print("starting model")
+model2 = ps.SINDy(optimizer=opt, feature_names=['i_d', 'i_q', 'i_0'] + TESTDATA['u_names'], feature_library=library)
+model2.fit(x_train, u=u_train, t=None, x_dot=xdot_train)
+model2.print()
+plot_coefs2(model2)
+plt.show()
